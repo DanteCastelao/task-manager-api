7a6bda0eab17650c5e8f3cd474762df8205afe8b
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2023-06-16 21:57:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { createTask, updateTask, deleteTask } = require('./controllers/taskContro
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server);
 
